8a3286555c22831329d19d46d5bae30bae5019ee
warmer <ken.kinion@gmail.com>
2017-06-23 07:05:34 -0400

    Breaks the proxy into more discrete components to assist with overriding behavior

--- http_proxy/reverse_http_proxy.rb
+++ http_proxy/reverse_http_proxy.rb
@@ -45,84 +45,128 @@ require 'optparse'
 #   working directory, then that file is served. Otherwise, the request
 #   passes through to the configured endpoint.
 #   `./reverse_http_proxy.rb -r [host/IP] -c [read-only cache dir]`
-class ReverseHttpProxy
-  def initialize(opts)
-    raise 'Must provide an options hash' unless opts.is_a? Hash
+module ReverseHttpProxy
+  # Handles reading and writing to incoming connections, and parsing and
+  # understanding the incoming request
+  class Client
+    attr_reader :verb, :resource, :version, :request, :headers
+    attr_reader :send_bytes, :content
 
-    @listen_port = opts[:listen_port] || 8080
-    @listen_host = opts[:listen_host] || '127.0.0.1'
-    @remote_host = opts[:remote_host]
-    @remote_port = opts[:remote_port] || 80
-    @cache_dir = opts[:cache_dir] || Dir.pwd
-    @cache_writeback = opts[:cache_writeback]
-  end
+    # Initializer requires a socket that is already established
+    def initialize(socket, opts = {})
+      @socket = socket
+      @verb = @resource = @version = @code = @message = @request = nil
+      @send_bytes = 0
+      @headers = []
+      @is_server = opts[:is_server]
+    end
 
-  def run!
-    start_server!
-    loop do
-      Thread.start(@proxy_server.accept) do |client|
-        action = resource = version = request = nil
-        send_bytes = 0
-        headers = []
-        # read lines from the socket
-        while line = client.gets
-          puts line
-          unless request
-            request = line
-            action, resource, version = line.split /\s+/
-          else
-            headers << line
-            if line.downcase.start_with? 'content-length: '
-              send_bytes = line.gsub(/^\S+\s+(\d+)\s+$/, '\1').to_i
-            end
-          end
-          # end of the HTTP request
-          break if line.strip == ''
+    # Read from our socket and make sense of the incoming request
+    def read_request!
+      # read lines from the socket
+      while line = @socket.gets
+        puts line
+        unless @request
+          parse_request(line)
+        else
+          parse_header(line)
         end
-        # read content if content is being sent
-        content = client.read(send_bytes) if send_bytes > 0
-        # now make the connection downstream
-        response_version = code = response_message = response = nil
-        receive_bytes = 0
-        response_headers = []
-        # open a connection to the remote host
-        remote = TCPSocket.new @remote_host, @remote_port
-        # send the request and the (joined) headers
-        remote.write request
-        headers.each {|header| remote.write header}
-        remote.write(content) if send_bytes > 0
-        # receive the response headers from downstream
-        while line = remote.gets
-          puts line
-          unless response
-            response = line
-            response_version, code, response_message = line.split(/\s+/, 3)
-          else
-            response_headers << line
-            if line.downcase.start_with? 'content-length: '
-              receive_bytes = line.gsub(/^\S+\s+(\d+)\s+$/, '\1').to_i
-            end
-          end
-          break if line.strip == ''
-        end
-        # read content if content is being returned
-        response_content = remote.read(receive_bytes) if receive_bytes > 0
-        # send them back to the client
-        client.puts response
-        response_headers.each {|header| client.puts header}
-        client.write(response_content) if receive_bytes > 0
-        # close the client when the remote closes its connection
-        remote.close
-        client.close
+        # end of the HTTP request
+        break if line.strip == ''
       end
+      read_content
+    end
+
+    def send_response(line)
+      @socket.puts(line)
+    end
+
+    def send_headers(headers)
+      headers.each {|header| @socket.puts header}
+    end
+
+    def send_content(content)
+      @socket.write(content) if content && content.length > 0
+    end
+
+    def close!
+      @socket.close
+    end
+
+    private
+
+    def parse_request(line)
+      @request = line
+      if @is_server
+        @version, @code, @message = line.split(/\s+/, 3)
+      else
+        @verb, @resource, @version = line.split(/\s+/, 3)
+      end
+    end
+
+    def parse_header(line)
+      @headers << line
+      if line.downcase.start_with? 'content-length: '
+        @send_bytes = line.gsub(/^\S+\s+(\d+)\s+$/, '\1').to_i
+      end
+    end
+
+    def read_content
+      # read content if content is being sent
+      @content = @socket.read(@send_bytes) if @send_bytes > 0
     end
   end
 
-  def start_server!
-    puts "Listening on #{@listen_host}:#{@listen_port}"
-    @proxy_server = TCPServer.new(@listen_host, @listen_port)
-  end
+  # Handles management of incoming connections, over-all proxy configuration,
+  # and high-level behavior of the proxy
+  class Server
+    def initialize(opts)
+      raise 'Must provide an options hash' unless opts.is_a? Hash
 
+      @listen_port = opts[:listen_port] || 8080
+      @listen_host = opts[:listen_host] || '127.0.0.1'
+      @remote_host = opts[:remote_host]
+      @remote_port = opts[:remote_port] || 80
+      @cache_dir = opts[:cache_dir] || Dir.pwd
+      @cache_writeback = opts[:cache_writeback]
+    end
+
+    def run!
+      start_server!
+      loop do
+        Thread.start(@proxy_server.accept) do |socket|
+          begin
+            request = Client.new(socket)
+            request.read_request!
+
+            response = Client.new(TCPSocket.new(@remote_host, @remote_port))
+            response.send_response(request.request)
+            response.send_headers(request.headers)
+            response.send_content(request.content)
+
+            response.read_request!
+
+            # send them back to the requester
+            request.send_response(response.request)
+            request.send_headers(response.headers)
+            request.send_content(response.content)
+
+            # close the requester when the remote closes its connection
+            response.close!
+            request.close!
+          rescue => e
+            puts e.message
+            puts e.backtrace
+          end
+        end
+      end
+    end
+
+    def start_server!
+      puts "Listening on #{@listen_host}:#{@listen_port}"
+      @proxy_server = TCPServer.new(@listen_host, @listen_port)
+    end
+  end
 end
 
 if $PROGRAM_NAME == __FILE__
@@ -155,6 +199,6 @@ if $PROGRAM_NAME == __FILE__
     puts opt_parse
     exit -1
   end
-  proxy = ReverseHttpProxy.new(options)
+  proxy = ReverseHttpProxy::Server.new(options)
   proxy.run!
 end
