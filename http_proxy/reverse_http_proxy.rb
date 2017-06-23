6a047f495404fd8016fb9567ee42beec6d2fdd48
warmer <ken.kinion@gmail.com>
2017-06-22 22:02:18 -0400

    Creates basic, functional proxy with no in-line request  modifications

--- http_proxy/reverse_http_proxy.rb
+++ http_proxy/reverse_http_proxy.rb
@@ -23,6 +23,7 @@
 # SOFTWARE.
 
 require 'socket'
+require 'optparse'
 
 # ReverseHttpProxy provides a lightweight, reverse HTTP proxy. This proxy can
 # be configured to have different behavior depending on the path of the
@@ -48,36 +49,112 @@ class ReverseHttpProxy
   def initialize(opts)
     raise 'Must provide an options hash' unless opts.is_a? Hash
 
-    @listen_port = opts[:listen_port] || 80
+    @listen_port = opts[:listen_port] || 8080
     @listen_host = opts[:listen_host] || '127.0.0.1'
     @remote_host = opts[:remote_host]
+    @remote_port = opts[:remote_port] || 80
     @cache_dir = opts[:cache_dir] || Dir.pwd
     @cache_writeback = opts[:cache_writeback]
   end
 
   def run!
-    @proxy_server = TCPServer.new(@listen_host, @listen_port)
+    start_server!
     loop do
       Thread.start(@proxy_server.accept) do |client|
-        request = nil
+        action = resource = version = request = nil
+        send_bytes = 0
         headers = []
         # read lines from the socket
         while line = client.gets
+          puts line
           unless request
             request = line
+            action, resource, version = line.split /\s+/
           else
             headers << line
+            if line.downcase.start_with? 'content-length: '
+              send_bytes = line.gsub(/^\S+\s+(\d+)\s+$/, '\1').to_i
+            end
           end
           # end of the HTTP request
-          break if line == ''
+          break if line.strip == ''
         end
+        # read content if content is being sent
+        content = client.read(send_bytes) if send_bytes > 0
+        # now make the connection downstream
+        response_version = code = response_message = response = nil
+        receive_bytes = 0
+        response_headers = []
         # open a connection to the remote host
+        remote = TCPSocket.new @remote_host, @remote_port
         # send the request and the (joined) headers
+        remote.write request
+        headers.each {|header| remote.write header}
+        remote.write(content) if send_bytes > 0
         # receive the response headers from downstream
+        while line = remote.gets
+          puts line
+          unless response
+            response = line
+            response_version, code, response_message = line.split(/\s+/, 3)
+          else
+            response_headers << line
+            if line.downcase.start_with? 'content-length: '
+              receive_bytes = line.gsub(/^\S+\s+(\d+)\s+$/, '\1').to_i
+            end
+          end
+          break if line.strip == ''
+        end
+        # read content if content is being returned
+        response_content = remote.read(receive_bytes) if receive_bytes > 0
         # send them back to the client
-        # optionally capture the response body
+        client.puts response
+        response_headers.each {|header| client.puts header}
+        client.write(response_content) if receive_bytes > 0
         # close the client when the remote closes its connection
+        remote.close
+        client.close
       end
     end
   end
+
+  def start_server!
+    puts "Listening on #{@listen_host}:#{@listen_port}"
+    @proxy_server = TCPServer.new(@listen_host, @listen_port)
+  end
+
+end
+
+if $PROGRAM_NAME == __FILE__
+  options = {
+    listen_port: 8080,
+    listen_host: '127.0.0.1',
+    remote_port: 80,
+    cache_writeback: false,
+    cache_dir: Dir.pwd,
+  }
+  args = ARGV.dup
+  opt_parse = OptionParser.new do |opts|
+    opts.banner = "Usage: ./#{File.basename(__FILE__)} [options]"
+    opts.on('-h', '--remote-host=HOST', String, 'Remote Host') do |host|
+      options[:remote_host] = host
+    end
+    opts.on('-p', '--remote-port=PORT', Integer, 'Remote Port') do |port|
+      options[:remote_port] = port
+    end
+  end
+  begin
+    opt_parse.parse!(args)
+  rescue OptionParser::InvalidOption => e
+    puts "Error - #{e.message}"
+    puts opt_parse
+    exit -1
+  end
+  unless options[:remote_host]
+    puts 'The remote host must be provided'
+    puts opt_parse
+    exit -1
+  end
+  proxy = ReverseHttpProxy.new(options)
+  proxy.run!
 end
