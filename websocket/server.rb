a346ba5d2b2d10f2adb0b50436e611a343a3535e
warmer <ken.kinion@gmail.com>
2017-07-17 07:50:51 -0400

    Adds barebones support for establishing a websocket connection

---
+++ websocket/server.rb
@@ -0,0 +1,250 @@
+#!/usr/bin/env ruby
+
+# MIT License
+#
+# Copyright (c) 2017 Kenneth Kinion
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+class WebSocketClient
+  def initialize(socket, opts = {})
+    @socket = socket
+    @logger = opts[:logger]
+  end
+
+  def serve
+    loop do
+      # BASE FRAMING PROTOCOL (from https://tools.ietf.org/html/rfc6455)
+      # Note that within each byte the MSB is to the LEFT so that
+      # the "opcode" in the first byte is masked by 0x0f and
+      # the FIN bit within the first byte is masked by (1 << 7), etc
+      #  0                   1                   2                   3
+      #  0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+      # +-+-+-+-+-------+-+-------------+-------------------------------+
+      # |F|R|R|R| opcode|M| Payload len |    Extended payload length    |
+      # |I|S|S|S|  (4)  |A|     (7)     |             (16/64)           |
+      # |N|V|V|V|       |S|             |   (if payload len==126/127)   |
+      # | |1|2|3|       |K|             |                               |
+      # +-+-+-+-+-------+-+-------------+ - - - - - - - - - - - - - - - +
+      # |     Extended payload length continued, if payload len == 127  |
+      # + - - - - - - - - - - - - - - - +-------------------------------+
+      # |                               |Masking-key, if MASK set to 1  |
+      # +-------------------------------+-------------------------------+
+      # | Masking-key (continued)       |          Payload Data         |
+      # +-------------------------------- - - - - - - - - - - - - - - - +
+      # :                     Payload Data continued ...                :
+      # + - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - +
+      # |                     Payload Data continued ...                |
+      # +---------------------------------------------------------------+
+      header, len = @socket.recv(2).unpack('C*')
+      @logger.debug "Header: #{header.inspect}"
+      fin = (header & (1 << 7)) > 0
+      opcode = (header & 0xf)
+      is_masked = (len & (1 << 7)) > 0
+      payload_size = (len & 0x7f)
+      if payload_size == 126
+        payload_size = @socket.recv(2).unpack('n').first
+      elsif payload_size == 127
+        size_words = @socket.recv(8).unpack('N')
+        payload_size = size_words[0] << 32 + size_words[1]
+      end
+      mask = is_masked ? @socket.recv(4).unpack('C*') : nil
+      @logger.info "Payload size: #{payload_size} B"
+      while payload_size > 0
+        to_read = [1024, payload_size].min
+        payload_size -= to_read
+
+        data = @socket.recv(to_read).unpack('C*')
+        data = data.each_with_index.map {|b, idx| b ^ mask[idx & 3] } if mask
+        str = data.pack('C*')
+        @logger.info "Received: #{str.inspect}"
+      end
+    end
+  end
+end
+
+# This is a stand-alone WebSocket implementation that uses only Ruby sockets
+# The emphasis on this implementation is on understanding what is happening
+# in the underlying requests, with a secondary goal of this being as concise
+# as possible and being contained within a single source file.
+#
+# References used to build this server include:
+# "Building a simple websockets server from scratch in Ruby"
+# http://blog.honeybadger.io/building-a-simple-websockets-server-from-scratch-in-ruby/
+#
+# "RFC 6455 - The WebSocket Protocol"
+# https://tools.ietf.org/html/rfc6455
+#
+# WebSocket Ruby Gem - "Universal Ruby library to handle WebSocket protocol"
+# https://github.com/imanel/websocket-ruby
+class WebSocketServer
+  require 'socket'
+  require 'logger'
+
+  # Configures the WebSocketServer, but does not start the server
+  # @params [Hash] opts - options hash supporting:
+  #   host: hostname from which to serve WebSockets
+  #   port: port from which to serve WebSockets
+  def initialize(opts = {})
+    @host = opts[:host]
+    @port = opts[:port]
+    @logger = opts[:logger]
+    unless @logger
+      @logger = Logger.new(STDOUT)
+      @logger.level = Logger::WARN
+    end
+  end
+
+  # Starts the WebSocket server, spawns a new thread for every request
+  def run!
+    start_server!
+    @server_thread = Thread.new do
+      loop do
+        Thread.start(@server.accept) do |socket|
+          begin
+            handle_request(socket)
+          rescue => e
+            @logger.error e.message
+            @logger.error e.backtrace
+          ensure
+            socket.close
+          end
+        end
+      end
+    end
+  end
+
+  def stop!
+    @server_thread.kill if @server_thread
+  end
+
+  private
+
+    # This handles the entire exchange between client and (if needed server)
+    # Note that this is deliberately broken into pieces to allow behavior to
+    # be overridden in pieces, as needed, depending on the application
+    #
+    # @param [TCPSocket] socket - the incoming socket from a proxy client
+
+  def handle_request(socket)
+    request = ''
+    while (line = socket.gets)
+      request += line
+      # end of the HTTP request
+      break if line.strip.empty?
+    end
+
+    if request =~ /^Sec-WebSocket-Key: (\S+)/
+      websocket_key = $1
+      @logger.info 'Received websocket key, establishing connection'
+      response_to_hash = "#{websocket_key}258EAFA5-E914-47DA-95CA-C5AB0DC85B11"
+      websocket_accept = Digest::SHA1.base64digest(response_to_hash)
+      response = <<-HEADER
+        HTTP/1.1 101 Switching Protocols
+        Upgrade: websocket
+        Connection: Upgrade
+        Sec-WebSocket-Accept: #{websocket_accept}
+
+      HEADER
+      response = response.split("\n", -1).map {|l| l.strip}
+      response = response.join("\r\n")
+      socket.write response
+      client = WebSocketClient.new(socket, logger: @logger)
+      # serves indefinitely
+      client.serve
+    else
+      @logger.warn 'Received invalid websocket request (missing key)'
+    end
+  end
+
+  # Starts the listening TCPServer. Note that this is abstracted to allow
+  # for overriding default behavior, like adding support for accepting
+  # connections over SSL/TLS
+  def start_server!
+    @logger.info "Listening on #{@host}:#{@port}"
+    @server = TCPServer.new(@host, @port)
+  end
+end
+
+
+# When this script is run directly, the proxy server is started with any
+# given default overrides.
+# For usage instructions, run this script with the '-?' flag
+if $PROGRAM_NAME == __FILE__
+  require 'optparse'
+  require 'webrick'
+  require 'logger'
+
+  options = {
+    host: '127.0.0.1',
+    web_port: 8080,
+    socket_port: 4568,
+  }
+  args = ARGV.dup
+  opt_parse = OptionParser.new do |opts|
+    opts.banner = "Usage: ./#{File.basename(__FILE__)} [options]"
+    opts.on('-h', '--host=HOST', String, 'Host serving content') do |host|
+      options[:host] = host
+    end
+    opts.on('-p', '--web-port=PORT', Integer, 'Port serving HTTP') do |port|
+      options[:web_port] = port
+    end
+    opts.on('-s', '--socket-port=PORT', Integer, 'Port serving WebSocket') do |port|
+      options[:socket_port] = port
+    end
+    opts.on_tail('-?', '--help', 'Display help') do
+      puts opt_parse
+      exit
+    end
+  end
+  begin
+    opt_parse.parse!(args)
+  rescue OptionParser::InvalidOption => e
+    $stderr.puts "Error - #{e.message}"
+    $stderr.puts opt_parse
+    exit -1
+  end
+
+  http_logger = Logger.new(STDOUT)
+  http_logger.level = Logger::WARN
+
+  http_opts = {
+    :Host => options[:host],
+    :Port => options[:web_port],
+    :DocumentRoot => './',
+    #:Logger => Logger.new(StringIO.new),
+    :Logger => Logger.new(STDOUT),
+  }
+  http_server = WEBrick::HTTPServer.new http_opts
+
+  proxy_logger = Logger.new(STDOUT)
+  proxy_logger.level = Logger::DEBUG
+  sock_opts = {
+    host: options[:host],
+    port: options[:socket_port],
+    logger: proxy_logger,
+  }
+  websocket_server = WebSocketServer.new sock_opts
+
+  [:INT, :TERM].each do |sig|
+    trap(sig) { http_server.stop; websocket_server.stop! }
+  end
+  websocket_server.run!
+  http_server.start
+end
