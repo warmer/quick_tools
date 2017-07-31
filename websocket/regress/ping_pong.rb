a9eed9d091c910d3c8f0960c3a260a8dfff38617
warmer <ken.kinion@gmail.com>
2017-07-31 07:51:18 -0400

    Ensures ping/pong test case is deterministic

--- websocket/regress/ping_pong.rb
+++ websocket/regress/ping_pong.rb
@@ -20,24 +20,33 @@ Harness.run_test do
     client_received = true
   end
 
+  log 'Send a ping from the client to the server'
+  client.send_frame(:ping, 'Hello?')
+
+  # Wait for activity on the client to complete
+  Timeout::timeout(1) do
+    loop do
+      break if client_received
+      sleep 0.1
+    end
+  end
+
+  scenario 'Send a ping from the server to the client'
+
   log 'Set a handler for receiving pongs on the server'
   server.on(:pong) do |_c, payload|
     log "Received pong with #{payload.string} as server"
     server_received = true
   end
 
-  log 'Send a ping from the client to the server'
-  client.send_frame(:ping, 'Hello?')
-
-  scenario 'Send a ping from the server to the client'
   server.connected_clients.each do |server_client|
     server_client.send_frame(:ping, 'ServerHello?')
   end
 
-  # Wait for activity on both the client and the server to complete
+  # Wait for activity on the server to complete
   Timeout::timeout(1) do
     loop do
-      break if client_received && server_received
+      break if server_received
       sleep 0.1
     end
   end
